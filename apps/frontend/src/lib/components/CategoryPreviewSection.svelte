9dfba6e03928df8c724c5d446f20f10b86aafb85
Miklos Daniel <daniel.miklos0@gmail.com>
2023-08-18 23:13:24 +0200

    serve logos from api routes

--- apps/frontend/src/lib/components/CategoryPreviewSection.svelte
+++ apps/frontend/src/lib/components/CategoryPreviewSection.svelte
@@ -7,26 +7,14 @@
 	};
 	type LogoProps = DeepRequired<ComponentProps<TeamElement>>;
 
-	import logo from '$lib/assets/logo_mini.svg?raw';
-
 	export let section: string = 'Foo Category';
-	export let logos: Array<LogoProps> = [
-		{ team: 'schdesign', logo, slug: 'schdesign' },
-		{ team: 'schdesign', logo, slug: 'schdesign' },
-		{ team: 'schdesign', logo, slug: 'schdesign' },
-		{ team: 'mgmt', logo, slug: 'schdesign' },
-		{ team: 'lego', logo, slug: 'schdesign' },
-		{ team: 'kir-dev', logo, slug: 'schdesign' },
-		{ team: 'kir-dev', logo, slug: 'schdesign' },
-		{ team: 'kir-dev', logo, slug: 'schdesign' },
-		{ team: 'kir-dev', logo, slug: 'schdesign' }
-	];
+	export let logos: Array<LogoProps> = [];
 </script>
 
 <section>
 	<h2 class="text-3xl font-semibold mb-4">{section}</h2>
 	<div class="grid gap-8" style="grid-template-columns: repeat(auto-fill,minmax(150px,1fr));">
-		{#each logos.sort((a, b) => a.team.localeCompare(b.team)) as logo}
+		{#each logos.sort((a, b) => a.teamName.localeCompare(b.teamName)) as logo}
 			<TeamElement {...logo} />
 		{/each}
 	</div>
--- packages/ui/components/TeamElement.svelte
+++ packages/ui/components/TeamElement.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-  export let team: string = "SCHdesign!";
-  export let slug: string = "schdesign";
-  export let logo: string = "";
+  export let teamName: string = "SCHdesign!";
+  export let href: string = "schdesign";
+
+  export let image: string = "";
 </script>
 
-<a href="/logok/{slug}">
+<a {href}>
   <div class="shadow-md rounded-lg p-2">
     <div class="aspect-square rounded-lg relative group transition-colors">
       <div class="svgContainer">
-        {@html logo}
+        <img
+          src={image}
+          class="object-contain w-full h-full"
+          alt={`${teamName} logo`}
+        />
       </div>
     </div>
     <div>
-      <h1 class="text-center">{team}</h1>
+      <h1 class="text-center">{teamName}</h1>
     </div>
   </div>
 </a>
