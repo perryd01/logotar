d90131df3b4274d32843a0378fd11872f37e3f32
Miklos Daniel <daniel.miklos0@gmail.com>
2023-08-28 23:05:33 +0200

    create team page with slug page also

---
+++ apps/admin/src/routes/korok/[slug]/+page.server.ts
@@ -0,0 +1,42 @@
+import db from '$lib/server/db';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const { slug } = params;
+	const team = await db.team.findFirst({
+		where: {
+			slug
+		},
+		include: {
+			Membership: {
+				include: {
+					User: {
+						select: {
+							id: true,
+							givenName: true,
+							surname: true,
+							internalId: true
+						}
+					}
+				}
+			},
+			Logos: true,
+			Group: {
+				select: {
+					id: true,
+					name: true,
+					slug: true
+				}
+			}
+		}
+	});
+
+	if (!team) {
+		throw error(404, 'Team not found');
+	}
+
+	return {
+		...team
+	};
+};
---
+++ apps/admin/src/routes/korok/[slug]/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import {
+		TableSearch,
+		TableHead,
+		TableHeadCell,
+		TableBody,
+		TableBodyRow,
+		TableBodyCell
+	} from 'flowbite-svelte';
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	let logoSearchTerm = '';
+
+	let userSearchTerm = '';
+</script>
+
+<svelte:head><title>{data.nameLong ?? data.name}</title></svelte:head>
+
+<h1>{data.name}</h1>
+{#if data.nameLong}
+	<small>{data.nameLong}</small>
+{/if}
+
+<p>id: {data.id}</p>
+<p>PéK id: {data.internalId}</p>
+
+<p>slug: {data.slug}</p>
+
+<section>
+	<h2>Logók</h2>
+	<TableSearch hoverable={true} bind:inputValue={logoSearchTerm}>
+		<TableHead>
+			<TableHeadCell>id</TableHeadCell>
+			<TableHeadCell>Név</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			{#each data.Logos as logo}
+				<TableBodyRow>
+					<TableBodyCell>
+						{logo.id}
+					</TableBodyCell>
+					<TableBodyCell>
+						{logo.name}
+					</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</TableSearch>
+</section>
+
+<section>
+	<h2>Tagok</h2>
+	<TableSearch hoverable={true} bind:inputValue={userSearchTerm}>
+		<TableHead>
+			<TableHeadCell>id</TableHeadCell>
+			<TableHeadCell>Név</TableHeadCell>
+			<TableHeadCell>Role</TableHeadCell>
+			<TableHeadCell>Title</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			{#each data.Membership as ms}
+				<TableBodyRow>
+					<TableBodyCell>
+						{ms.User?.id}
+					</TableBodyCell>
+					<TableBodyCell>
+						{ms.User?.surname}
+						{ms.User?.givenName}
+					</TableBodyCell>
+					<TableBodyCell>
+						{ms.role}
+					</TableBodyCell>
+					<TableBodyCell>
+						{ms.title.join(', ')}
+					</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</TableSearch>
+</section>
+
+<style lang="postcss">
+	section {
+		@apply my-8;
+	}
+</style>
